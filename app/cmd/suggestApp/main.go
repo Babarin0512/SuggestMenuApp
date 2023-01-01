6b9e518aa89455109289b2a39c5e25d11d33e98f
Hirotaka Baba <babarin0512business@gmail.com>
2023-01-01 14:56:37 +0900

    テーブルの値を取得してコンソールに表示する関数の作成

--- app/cmd/suggestApp/main.go
+++ app/cmd/suggestApp/main.go
@@ -3,11 +3,16 @@ package main
 import (
 	"log"
 	"net/http"
+	sql "github.com/Babarin0512/SuggestMenuApp/tree/main/app/pkg/backend/sql" //パッケージ名が長いので別名で参照する
 )
  
 func main() {
-	port := "8080"
+	sql.ShowShokuzaiID()
+
+
+
+	/*port := "8080"
 	http.Handle("/", http.FileServer(http.Dir("./docs/")))
 	log.Printf("Listen on port: %s", port)
-	http.ListenAndServe(":"+port, nil)
+	http.ListenAndServe(":"+port, nil)*/
 }
--- app/pkg/backend/sql/sqlquely.go
+++ app/pkg/backend/sql/sqlquely.go
@@ -1,2 +1,46 @@
-sqlquely.go
-dlajlad
+package sql 
+
+import{
+	"fmt"
+	"database/sql"
+	"log"
+	_ "github.com/go-sql-driver/mysql"
+}
+
+dbDriver := "mysql"
+dbUser := "test_user"
+dbPass := "abcd0512"
+dbName := "test_database"
+dbRootPass := "root_abcd0512"
+//dbAddress := 
+
+func ShowShokuzaiID(){
+	db, err := sql.Open(dbDriver, dbUser+":"+dbPass+"@/"+dbName)//コンテナ間接続のためアドレスをしていする
+    if err != nil {
+        return nil, err
+    }
+    defer db.Close()
+
+	//
+	rows, err := db.Query("select shokuzai_id from shokuzai_refrigerator")
+	if err != nil {
+		log.Println(err)
+	}
+
+	//goのスライスにdbから取得した値を格納する
+	var shokuzaiID []ID
+	for rows.storeInSlice(){
+		//スライスの中身を初期化する
+		id := ID{}
+		if err := rows.Scan(&id.shokuzai_id); err != nil {
+			log.Println(err)
+		}
+		//取得した値（行）をshokuzaiID(構造体)に格納する
+		shokuzaiID = append(shokuzaiID.id)
+	}
+
+	//構造体として取得したテーブル(hokuzai_refrigerator)の中身をコンソールに出力する
+	for _,i := range shokuzaiID{
+		fmt.Println("shokuzai_id", i.shokuzai_id)
+	}
+}
